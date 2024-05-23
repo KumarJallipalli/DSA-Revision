03a0ee8243236fe371c1db343ff2538f29f64421
Siva Jallipalli <kumar.jallipalli@gmail.com>
2024-05-23 17:44:36 +0530

    intro Class in Java

---
+++ 01. Intro/Intro.java
@@ -0,0 +1,29 @@
+/**
+ * 01Intro
+*/
+
+/**
+ *  - Flowcharts --> Diagramatic representation of sequence of logical steps of a program
+ *  - Psuedocode --> Step-by-Step representation of an algorithm in plain english [with No syntax rules]
+ *  - Algorithm --> A finite set of instructions that are carried out in a specified order to perform a specific task
+ * 
+ *  - All the above theree are Language Independent
+*/
+
+
+/**
+ *  Boilerplate Code:
+ *      - A piece of code that is repeated in multiple places with little or no variation
+*/
+
+// This is the boilerplate code
+public class Intro {
+    public static void main(String[] args) {
+        /**
+         *  - main function is the Entry point for JAVA programs
+         *  - i.e., Program execution in JAVA starts from main function
+         * 
+         *  - filenmae & public class name MUST be SAME in Java
+        */
+    }
+}
